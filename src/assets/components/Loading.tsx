99901b25598a24c72547bc53f82a943df03f41a4
NestorSanchez <nesaenz.69@gmail.com>
2023-06-30 12:58:12 -0500

    create component Loading

---
+++ src/assets/components/Loading.tsx
@@ -0,0 +1,11 @@
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { stylesLoading } from '../../utils/Styles'
+
+export default function MyComponent():JSX.Element {
+  const classes = stylesLoading()
+  return (
+    <div className={classes.container}>
+      <CircularProgress disableShrink className={classes.colorCircular} />
+    </div>
+  );
+}
